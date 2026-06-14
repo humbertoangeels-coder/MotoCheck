f76b4c66cb23f0f02f6e1f29a5fa5b0ba999c9e0
humbertoangeels-coder <humbertoangeels@gmail.com>
2026-06-13 20:38:32 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'routes/app_routes.dart';
 
+void main() {
+  runApp(const MotoCheckApp());
+}
+
+class MotoCheckApp extends StatelessWidget {
+  const MotoCheckApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: AppRoutes.routes,
+      initialRoute: '/login',
+    );
+  }
+}
