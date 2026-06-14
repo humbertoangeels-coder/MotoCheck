ac0e21e96d5857d44b91716aefe4faf620eeea6d
humbertoangeels-coder <humbertoangeels@gmail.com>
2026-06-13 20:50:14 -0500

    cambio login y main

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
-import 'routes/app_routes.dart';
+import 'screens/login/login_screen.dart'; // Asegúrate de importar el archivo que creaste
 
 void main() {
-  runApp(const MotoCheckApp());
+  runApp(MyApp());
 }
 
-class MotoCheckApp extends StatelessWidget {
-  const MotoCheckApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Mantenimiento App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginScreen(), // Define el Login como la primera pantalla
       debugShowCheckedModeBanner: false,
-      routes: AppRoutes.routes,
-      initialRoute: '/login',
     );
   }
 }
